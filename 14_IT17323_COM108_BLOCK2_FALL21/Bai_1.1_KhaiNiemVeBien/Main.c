e281344c42c6de6bb83c097be1f497325acfbd63
Dungna89 <dungna29@fpt.edu.vn>
2021-11-20 18:22:13 +0700

    1

---
+++ 14_IT17323_COM108_BLOCK2_FALL21/Bai_1.1_KhaiNiemVeBien/Main.c
@@ -0,0 +1,64 @@
+﻿#include <stdio.h>
+/*
+ * BÀI 1.1 KHÁI NIỆM VỀ BIẾN
+ * 1. Biến dùng để lữu trữ giá trị trong lập trình
+ * 2. Biến được lưu vào RAM máy tính khi chương trình bắt đầu chạy và sẽ mất đi khi dừng chương trình
+ * 3. Biến cần phải có kiểu dữ liệu ví dụ: int, float, char......
+ */
+double _diemJava = 9.2;//Khai báo biến toàn cục
+int main()
+{/*
+	 * Phần 1: Công thức khai báo biến
+	 * + <Kiểu dữ liệu> <Tên biến>;//Cách khai báo 1
+	 * + <Kiểu dữ liệu> <Tên biến 1>,<Tên biến 2>,<Tên biến 3>;//Khai báo nhiều biến có cùng kiểu dữ liệu
+	 * + <Kiểu dữ liệu> <Tên biến> =(toán tử gán) <giá trị>;
+	 *
+	 * <Kiểu dữ liệu>: Dùng để định nghĩa kiểu dữ liệu cho giá giá trị cần lưu trữ.
+	 */
+
+	 //Ví dụ:	
+	int a;//Khai báo 1 biến a có kiểu dữ liệu số nguyên và không khởi tạo giá trị ban đầu
+	a = 5;//Gán giá trị cho biến a thông qua toán tử =
+
+	int b = 9;//Khai báo 1 biến b có kiểu dữ liệu số nguyên và có khởi tạo giá trị ban đầu
+
+	double c = 9.2;
+
+	int d1, d2, d3, d4 = 10;//Khai báo nhiều biến có cùng kiểu dữ liệu
+
+	int tuoi = 18;
+
+	/*
+	 * Phần 2: Đặt tên biến
+	 * - Tên biến đặt dễ hiểu và đúng nghĩa
+	 * - Tên biến hạn chế đặt bằng tiếng việt khi đi làm về sau
+	 * - Không đặt tên biến gây hiểu nhầm.
+	 * - Tên biến có phân biệt chữ hoa chữ thường
+	 * Gồm 3 cách đặt tên biến với 2 từ trở lên
+	 * - C1: normal: sinhvien - thông thường tên biến sẽ đặt kiểu nomarl
+	 * - C2: CamelCase: SinhVien
+	 * - C3: Under_Score: sinh_Vien
+	 */
+	 /*
+	  * Phần 3: Các kiểu dữ liệu trong lập trình
+	  * Số nguyên: short,long, int
+	  * Số thực: float,double
+	  * Ký tự: char (Chỉ có thể lưu trữ được 1 ký tự)
+	  * Chuỗi: String (Ngôn ngữ C không có String)
+	  * Logic: Boolean trả ra True hoặc False nhưng trong C sẽ trả ra 0 và 1
+	  */
+	short  s1 = 999;// Từ -32768 đến lớn nhất là 32767
+	long l1 = 99;
+	int i1 = 100;
+
+	float f1 = 2.6;
+	double diemC = 2.1;
+
+	char c1 = '5';
+	char c2 = 'a';
+	/*
+	 * Có 2 loại biến toàn cục và cục bộ:
+	 *	+ Biến toàn cục là khai báo bên ngoài hàm
+	 *	+ Cuc bộ là bên trong của hàm
+	 */
+}
---
+++ 14_IT17323_COM108_BLOCK2_FALL21/Bai_1.2_CauLenhXuatRaManHinh/Main.c
@@ -0,0 +1,47 @@
+﻿#include <stdio.h>
+/*
+ * Bài 1.2 Câu lệnh printf(); giúp in ra màn hình
+ */
+int main()
+{
+	/*
+	 * Phần 1: Cách sử dụng
+	 * printf("Chuỗi giá trị");// Dùng để in 1 chuỗi giá trị ra màn hình
+	 * printf("%d",<giá trị>);// In giá trị của biến ra màn hình
+	 * %d- Số nguyên %f- số thực %s- String %c- ký tự %lf - double
+	 */
+
+	printf_s("Nam nay toi se hoc lap trinh C \n");
+	int namHienTai = 2021, namSau = 2022;
+	printf_s("Nam %d toi se hoc xong ngon ngu C \n", namHienTai);
+
+	printf_s("Sau nam %d sang nam %d toi se hoc lap trinh JAVA hoac C# \n", namHienTai, namSau);
+
+	//Ctrl  + F5 = chạy
+	/*
+	 * Bài 1: Viết 1 chương trình khai báo 3 năm sinh
+	 * sau đó in 3 năm sinh ra màn hình theo công thức sau:
+	 * Nam Sinh 1: 1999
+	 * Nam Sinh 2: 2000
+	 * Nam Sinh 3: 2001
+	 *
+	 * Bài 2: Viết 1 chương trình in điểm môn C ra màn hình
+	 * theo công thức sau
+	 * Diem C cua ban la: 8.6
+	 *
+	 * Bài 3: Viết 1 chương trình in 1 ký tự bất kì ra màn hình
+	 *
+	 */
+	//Bài 1:
+	printf_s("==============Bai 1====================\n");
+	int y1 = 1999, y2=2000, y3=2001;
+	printf_s("Nam Sinh 1: %d \n", y1);
+	printf_s("Nam Sinh 2: %d \n", y2);
+	printf_s("Nam Sinh 3: %d \n", y3);
+	printf_s("==============Bai 2====================\n");
+	double diemC = 8.6;
+	printf_s("Diem C cua ban la: %.1lf \n", diemC);
+	printf_s("==============Bai 3====================\n");
+	char c = '%';
+	printf_s("Ky tu %c la cai toi muon \n", c);	
+}
