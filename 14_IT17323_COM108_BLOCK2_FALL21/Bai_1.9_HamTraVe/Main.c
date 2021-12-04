316dae950e19a39f2641ba5db80af8e783ce9c7b
Dungna89 <dungna29@fpt.edu.vn>
2021-12-04 18:29:50 +0700

    1

---
+++ 14_IT17323_COM108_BLOCK2_FALL21/Bai_1.9_HamTraVe/Main.c
@@ -0,0 +1,45 @@
+﻿#include <stdio.h>
+/*
+* Hàm trả về
+*	1. Không tham số
+*	2. Có tham số
+* Cách sử dụng:
+*	1. Tên hàm phải có nghĩa
+*	2. Tên hàm luôn có động từ đứng trước:
+*		Ví dụ: tinhTong(),layDsSinhVien, getLstSinhVien.....
+  Công thức:
+  <Kiểu dữ liệu> <Tên hàm> (<Các tham số truyền vào>)
+  {
+	return <Giá trị phải đúng với kiểu dữ liệu của hàm>;
+  }
+*/
+void tinhTong2A(int a, int b)
+{
+	printf_s("%d + %d = %d \n", a, b, a + b);
+}
+int tinhTong2B(int a, int b)
+{
+	printf_s("%d + %d = %d \n", a, b, a + b);
+	return a + b;//Không được code sau return
+}
+char ham1()
+{
+	char c = '/';
+	return c;
+}
+double ham2()
+{
+	return 8.6;
+}
+//Buổi sau giải thử bài 7 bằng hàm trả về.
+int main()
+{
+
+	tinhTong2A(10, 10);
+	tinhTong2B(5,7);//Không thể tự in giá trị ra màn hình
+	printf_s("Tong = %d\n", tinhTong2B(5, 7)+2000);
+	int a = tinhTong2B(5, 7);
+
+	//Lưu ý: Khi mới học thì hãy con cả hàm trả về là 1 giá trị.
+	
+}
---
+++ 14_IT17323_COM108_BLOCK2_FALL21/CHUA_BAI_LAB12/Main.c
@@ -0,0 +1,45 @@
+﻿#include <stdio.h>
+
+void bai5()
+{
+	//In khoảng bảng cửu chương
+	int khoang1, khoang2;
+	printf_s("Moi nhap KHOANG1: ");
+	scanf_s("%d", &khoang1);//8
+	printf_s("Moi nhap KHOANG2: ");
+	scanf_s("%d", &khoang2);//5
+	//Sử dụng thuật toán hoán vị
+	if (khoang1> khoang2)
+	{
+		int temp = khoang1;
+		khoang1 = khoang2;
+		khoang2 = temp;
+	}
+	for (int i = khoang1; i <= khoang2; ++i)
+	{
+		for (int j = 1; j <= 10; ++j)
+		{
+			printf_s("%d x %d = %d \n", i, j, i * j);
+		}
+	}
+}
+void Bai7()
+{
+	//Nhập một dãy số do người dùng rồi cộng tổng
+	int so, soLuong, TongCacSo = 0;
+	printf_s("Ban muon nhap bao nhieu: ");
+	scanf_s("%d", &soLuong);
+	for (int i = 0; i < soLuong; ++i)
+	{
+		printf_s("Moi ban nhap so thu %d: ", i);
+		scanf_s("%d", &so);
+		//TongCacSo = TongCacSo + so;
+		TongCacSo += so;
+	}
+	printf_s("Tong cac so: %d\n", TongCacSo);
+}
+
+int main()
+{
+	Bai7();
+}
