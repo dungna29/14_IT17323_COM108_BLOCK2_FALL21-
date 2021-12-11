7f2c13bb9137746ba463696fc7ea0b5e761c28b1
Dungna89 <dungna29@fpt.edu.vn>
2021-12-11 18:19:26 +0700

    1

--- 14_IT17323_COM108_BLOCK2_FALL21/BAI_TAP_MAU_MANG/Main.c
+++ 14_IT17323_COM108_BLOCK2_FALL21/BAI_TAP_MAU_MANG/Main.c
@@ -3,9 +3,11 @@ int arrNumberFpoly[] = { 88,7,9,6,5,8 };//Khai báo biến toàn cục
 void Bai1a();
 void Bai1b();
 void Bai1c();
+void Bai2a();
+void Bai8();
 int main()
 {
-	Bai1b();
+	Bai8();
 }
 void Bai1a()
 {
@@ -28,15 +30,72 @@ void Bai1c()
 		}
 	}
 }
+void Bai2a()
+{
+	int arrNumbers[100], soLuong;
+	printf_s("Moi ban nhạp SL: ");
+	scanf_s("%d", &soLuong);
+	for (int i = 0; i < soLuong; ++i)
+	{
+		printf_s("Index [%d] = ", i);
+		scanf_s("%d", &arrNumbers[i]);
+	}
+	for (int i = 0; i < soLuong; ++i)
+	{
+		printf_s("Index [%d] = %d \n", i, arrNumbers[i]);
+	}
+}
+void Bai8()
+{
+	// Bài 8: Nhập nhiệt độ các người đi xét nghiệm liệt kê vị trí thứ tự và những người có nhiệt độ sốt từ 38.1 độ trở lên.
+	double arrNhietDo[100];
+	int soLuong;
+	printf_s("So luong?: ");
+	scanf_s("%d", &soLuong);
+	for (int i = 0; i < soLuong; ++i)
+	{
+		printf_s("Nhap nhiet do nguoi thu %d: ", i);
+		scanf_s("%lf", &arrNhietDo[i]);
+	}
+	for (int i = 0; i < soLuong; ++i)
+	{
+		if (arrNhietDo[i] >= 38.1)
+		{
+			printf_s("Canh bao nguoi o vi tri thu %d co dau hieu sot = %.1lf \n", i, arrNhietDo[i]);
+		}
+		
+	}
+	double nhietDoCaoNhat = arrNhietDo[0];
+	for (int i = 1; i < soLuong; ++i)
+	{
+		if (nhietDoCaoNhat < arrNhietDo[i])
+		{
+			nhietDoCaoNhat = arrNhietDo[i];
+		}
+	}
+	printf_s("Nhiet do nguoi cao nhat = %1.lf", nhietDoCaoNhat);
+	
+}
 /*
  * Bài 1: Khai báo 1 mảng số nguyên gồm các phần tử sau
- * int arrNumberFpoly[] = {88,7,9,6,5,8};	 *
+ * int arrNumberFpoly[] = {88,7,9,6,5,8};
  *  - In số 5 ra màn hình bằng câu printf.
  *  - In tất cả các phần tử có ở trong mảng ra màn hình
  *  - In tất cả các giá trị chẵn ra màn hình.
  *
  *	Bài 2: Cho người dùng nhập vào 1 mảng số nguyên và in tổng dãy số người dùng đã nhập ra màn hình.
  *
+ *	Bài 3: Cho người dùng nhập vào 1 mảng số nguyên rồi in ra số lớn nhất trong mảng
  *
+ *	Bài 4: Cho người dùng nhập vào 1 mảng số nguyên cho in ra các số chẵn lẻ trên 2 dòng riêng biệt.
+ *
+ *	Bài 5: Cho người dùng nhập vào 1 mảng chẵn nếu là số lẻ nhập vào bắt nhập lại.
+ *
+ *  Bài 6: Nhập vào cân nặng các người yêu cũ tìm ra vị trí người có số cân nặng bé nhất.
+ *
+ *	Bài 7: Nhập vào năm sinh các người yêu cũ in ra người yêu cũ có tuổi lớn nhất và thứ tự nhập vào.
+ *	Ví dụ: Năm 2000 Tuổi 21 Người yêu thứ 3.
+ *
+ *	Bài 8: Nhập nhiệt độ các người đi xét nghiệm liệt kê vị trí thứ tự và những người có nhiệt độ sốt từ 38.1 độ trở lên.
  *
  */
---
+++ 14_IT17323_COM108_BLOCK2_FALL21/Bai_2.2_StringTrongC/Main.c
@@ -0,0 +1,53 @@
+﻿#include <stdio.h>
+/*
+ * Chuỗi trong C nó là 1 mảng ký tự
+ * Java -String, C# - string (chuỗi)
+ */
+
+int main()
+{
+	//Chu Van Duy = Chuỗi = Tập hợp của nhiều ký tự = Mảng ký tự
+
+	char arr[] = { 'D','U','Y' };
+	//Khái báo 1 mảng kiểu char
+	char arrName[100];
+	printf_s("Moi ban nhap ten: ");
+	//scanf_s("%s", arrName, 100); Nó ko đọc được khoảng trắng tiếp theo
+	//scanf_s("%[^\n]", arrName, 100);//Regex
+	//fgets(arrName, 100, stdin);
+	gets_s(arrName, 100);//Cách này thường dùng khi đi thi
+	printf_s("Chao ban %s", arrName);
+	/*
+	 * Nhập các thông tin SV FPOLY sau:
+	 *	1. Tên đầy đủ
+	 *	2. Năm sinh
+	 *	3. Sdt
+	 *	4. Điểm Thi C
+	 *	Sau đó in ra màn hình như sau:
+	 *	Tên + Năm sinh + Tuổi + SDT + Diểm C - (Qua môn hoặc học lại)
+	 */
+
+	char ten[100],sdt[100];
+	int ns;
+	double diemThiC;
+	printf_s("Moi ban nhap Ten: ");
+	gets_s(ten, 100);
+	printf_s("Moi ban nhap sdt: ");
+	gets_s(sdt, 100);
+	printf_s("Moi ban nhap ns: ");
+	scanf_s("%d", &ns);
+	printf_s("Moi ban nhap diemC: ");
+	scanf_s("%lf", &diemThiC);
+	printf_s("Ten: %s ", ten);
+	printf_s("Sdt: %s ", sdt);
+	printf_s("Ns: %d ", ns);
+	printf_s("Tuoi: %d ", 2021-ns);
+	printf_s("DiemC: %.1lf ", diemThiC);
+	if (diemThiC>= 5)
+	{
+		printf_s("Qua Mon");
+		return;
+	}
+	printf_s("Hoc Lai");
+	
+}
